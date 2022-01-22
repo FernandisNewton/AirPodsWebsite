83db152d73a9ef197f8896d5805fce8eb20a3739
Newton Fernandis <newtonb.is18@sahyadri.edu.in>
2022-01-22 10:21:50 +0530

    added delay

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import React,{useEffect} from "react";
 import './App.css';
-
+import vid from './AirPods.mp4';
+import ScrollMagic from "scrollmagic";
+import ProgressBar from "react-scroll-progress-bar";
 function App() {
+
+  useEffect(() => {
+    const intro = document.querySelector('.intro');
+    const video = document.querySelector('video');
+    const section = document.querySelector('section');
+
+    const controller = new ScrollMagic.Controller();
+
+    const scene = new ScrollMagic.Scene({
+      duration:30000,
+      targetElement:intro,
+      triggerHook:0,
+    }).setPin(intro).addTo(controller);
+
+    let accel = 0.1;
+    let scrPos = 0;
+    let delay=0;
+
+    scene.on("update",(e)=>{
+      scrPos = e.scrollPos / 1000;
+    })
+
+    setInterval(()=>{
+      delay += (scrPos - delay) * accel;
+      video.currentTime = delay;
+    },33)
+
+  },[]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ProgressBar />
+      <div className = "intro" id="id">
+        <video src = {vid} muted></video>
+      </div>
+      <section>
+        <h1>THANK YOU FOR WATCHING</h1>
+      </section>
     </div>
   );
 }
